c709e7d5ffd17a8d62c14921fc73cb03577d0f92
gl10712 <gary.laughman@kroger.com>
2017-05-12 21:57:39 -0400

    Initial Commit

---
+++ src/main/java/com/gary/GaryApplication.java
@@ -0,0 +1,12 @@
+package com.gary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaryApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(GaryApplication.class, args);
+    }
+}
